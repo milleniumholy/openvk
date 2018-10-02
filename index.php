d7d84c46c6a1ecd36ef4dfa30e0dac97d517bb15
Artem Prihodov <40433298+Infinite96x@users.noreply.github.com>
2018-10-02 15:31:23 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>OpenVK 3.0</title>
+  <meta charset="utf-8" />
+  <? include "vanilla.php"; ?>
+</head>
+<body>
+  <h1>OpenVK 3.0</h1>
+  <span>мы удалили всё лишнее. мы удалили всё)))</span>
+</body>
+</html>
